2537102ae98d840f8c7fa9a55ffa42a3d3c92ab1
kaan343534 <uranka@hotmail.com>
2021-12-17 22:02:50 +0300

    First commit

---
+++ components/Login.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import { useMoralis } from 'react-moralis'
+
+const Login = () => {
+    const { authenticate } = useMoralis()
+
+    return (
+        <div className="bg-black relative ">
+            <h1>Login Screen</h1>
+            <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+                <div className="object-cover rounded-full">
+                <Image src="https://links.papareact.com/3pi" width={200} height={200} />
+                </div>
+                <button  className="bg-yellow-500 rounded-lg p-5 font-bold  animate-pulse" onClick={authenticate}>Login into App</button>
+            </div>
+            <div className="w-full h-screen">
+                <Image src="https://links.papareact.com/55n" layout="fill" objectFit="cover" />
+            </div>
+        </div>
+    )
+}
+
+export default Login
